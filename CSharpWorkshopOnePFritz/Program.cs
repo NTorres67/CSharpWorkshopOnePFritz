61041544f1daa3f84a471dc84cd47df32b578888
NTorres67 <NTorres1967@gmail.com>
2018-05-18 10:50:00 -0400

    Initial Commit

--- CSharpWorkshopOnePFritz/Program.cs
+++ CSharpWorkshopOnePFritz/Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharpWorkshopOnePFritz
+namespace CSharpWorkshopOnePFritz 
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            var stuff = new UserInterface.Foo();
+            var mariasRealCar = new Tesla.ModelS
         }
     }
+}
+
+
+namespace CSharpWorkshopOnePFritz.UserInterface
+{
+    public class Foo
+    {
+        public Ford.Fusion myCar;
+        public Ford.Fusion MariasCars;
+    }
+}
+
+namespace Ford {
+
+    public class Fusion
+    {
+        public bool StartEngine()
+        {
+            return true;
+        }
+    }
+
 }
